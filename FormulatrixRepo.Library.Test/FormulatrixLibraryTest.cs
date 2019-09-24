dc9b47faa4606e5b5f17dfd0ad208d5fc7e7ca98
muhammad akbar yasin <akbar.yasin@m2.formulatrix.com>
2019-09-24 19:31:45 +0700

    Test cases lucu2an
    Signed-off-by: muhammad akbar yasin <m.akbar.yasin@gmail.com>

--- FormulatrixRepo.Library.Test/FormulatrixLibraryTest.cs
+++ FormulatrixRepo.Library.Test/FormulatrixLibraryTest.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using NUnit.Framework;
-using FormulatrixRepo.Library;
+﻿using NUnit.Framework;
 
 namespace FormulatrixRepo.Library.Test
 {
   [TestFixture]
   public class FormulatrixLibraryTest
   {
-    
+    // check the content of the Register input must be the same as output of the Retrieve content
+    [Test]
+    public void FormulatrixRepo_Register_Then_Retrieve_String()
+    {
+      string inputName = "test";
+      int inputContentType = 1;
+      string inputContent = "{\"test\" : \"test\"}";
+      FormulatrixRepo<string>.Register( inputName, inputContent, inputContentType );
+      string outputContent = FormulatrixRepo<string>.Retrieve<string>( inputName );
+
+      // deregister so that this test case can repeated due to existance of an item
+      FormulatrixRepo<string>.Deregister( inputName );
+      Assert.AreEqual(inputContent, outputContent);
+    }
+
+    // check the content of the Register input must be the same as output of the Retrieve content
+    [Test]
+    public void FormulatrixRepo_Register_Then_Retrieve_Int()
+    {
+      string inputName = "test";
+      int inputContentType = 1;
+      int inputContent = 101;
+      FormulatrixRepo<int>.Register( inputName, inputContent, inputContentType );
+      int outputContent = FormulatrixRepo<int>.Retrieve<int>( inputName );
+
+      // deregister so that this test case can repeated due to existance of an item
+      FormulatrixRepo<int>.Deregister( inputName );
+      Assert.AreEqual( inputContent, outputContent );
+    }
+
+    [Test]
+    public void FormulatrixRepo_Register()
+    {
+
+    }
   }
 }
