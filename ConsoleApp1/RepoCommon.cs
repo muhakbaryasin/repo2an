25831798543740e21e26ddc7a55b6bcaca887320
muhammad akbar yasin <akbar.yasin@m2.formulatrix.com>
2019-09-24 15:23:11 +0700

    Misahin class" nya doang. Ke FormulatrixRepos.cs dan RepoCommon.cs
    Signed-off-by: muhammad akbar yasin <m.akbar.yasin@gmail.com>

---
+++ ConsoleApp1/RepoCommon.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+  class RepoCommon
+  {
+    public static bool FileNameIsValid( string itemName )
+    {
+      string fileNamePattern = @"^[\w\-. ]+$";
+      return System.Text.RegularExpressions.Regex.IsMatch( itemName, fileNamePattern );
+    }
+
+    public static bool XmlIsValid( string itemContent )
+    {
+      return true;
+    }
+
+    public static bool JsonIsValid( string itemContent )
+    {
+      return true;
+    }
+
+    public static void SaveToFile( string fileName, string itemContent )
+    {
+      try
+      {
+        // writing file stream procedure
+        System.IO.File.WriteAllText( @"" + fileName, itemContent );
+      }
+      catch( Exception )
+      {
+        // writing file stream failed handling procedure
+        throw;
+      }
+    }
+
+    public static bool FileExists( string fileName )
+    {
+      return System.IO.File.Exists( fileName );
+    }
+
+  }
+}
