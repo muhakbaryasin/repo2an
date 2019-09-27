4032cf7e0242bea11eeffa16a93e87eb01520742
muhammad akbar yasin <akbar.yasin@m2.formulatrix.com>
2019-09-27 18:47:30 +0700

    Beneran
    Signed-off-by: muhammad akbar yasin <m.akbar.yasin@gmail.com>

---
+++ ConsoleRepoApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using FormulatrixRepoLibrary;
+
+namespace ConsoleRepoApp
+{
+  class Program
+  {
+    static void Main( string[] args )
+    {
+      FormulatrixRepo fmlxRepo = new FormulatrixRepo();
+      int a = 1;
+      string b = "bbb";
+
+      fmlxRepo.Register( "a", a );
+      fmlxRepo.Register( "b", b );
+      A objA = new A() { nama = "myname", id = "1234" };
+      fmlxRepo.Register( "objectA", objA );
+
+      // foreach( var f in fmlxRepo.GetType( "objectA" ).GetFields().Where( f => f.IsPublic ) )
+      foreach( var f in fmlxRepo.GetType( "objectA" ).GetFields() )
+      {
+        Console.WriteLine(
+            String.Format( "Name: {0} Value: {1}", f.Name, f.GetValue( fmlxRepo.Retrieve( "objectA" ) ) )
+        );
+      }
+
+      var retObj = Convert.ChangeType( fmlxRepo.Retrieve( "objectA" ), fmlxRepo.GetType( "objectA" ) );
+      Console.ReadLine();
+    }
+  }
+
+  class A
+  {
+    public string nama;
+    public string id;
+  }
+}
---
+++ FormulatrixRepoLibrary/FormulatrixRepo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FormulatrixRepoLibrary
+{
+  public class FormulatrixRepo
+  {
+    // IDictionary<string, RepoWrapper> RepoCollection = new Dictionary<string, RepoWrapper>();
+    IDictionary<string, RepoWrapper> RepoCollection = new Dictionary<string, RepoWrapper>();
+    // public void Register( string itemName, T data )
+    public void Register( string itemName, object Object )
+    {
+      if( !IsItemNameValid( itemName ) )
+        throw new Exception( $"{itemName} is an invalid item name." );
+
+      // var objA = new sfdsf( asdfdsf );
+
+      RepoCollection.Add( itemName, new RepoWrapper( Object ) );
+    }
+
+    public object Retrieve( string itemName )
+    {
+      if( !IsItemNameValid( itemName ) )
+        throw new Exception( $"{itemName} is an invalid item name." );
+
+      return RepoCollection[itemName].Object();
+    }
+
+    public Type GetType( string itemName )
+    {
+      if( !IsItemNameValid( itemName ) )
+        throw new Exception( $"{itemName} is an invalid item name." );
+
+      return RepoCollection[itemName].Type();
+    }
+
+    public void Deregister( string itemName )
+    {
+      if( !IsItemNameValid( itemName ) )
+        throw new Exception( $"{itemName} is an invalid item name." );
+
+      RepoCollection.Remove( itemName );
+    }
+
+    private bool IsItemNameValid( string itemName )
+    {
+      string namePattern = @"^[\w\- ]+$";
+      return System.Text.RegularExpressions.Regex.IsMatch( itemName, namePattern );
+    }
+    private void Initialize() { }
+  }
+
+  interface IData
+  {
+    void Set( object Object );
+    object Object();
+    Type Type();
+  }
+
+  public class RepoWrapper : IData
+  {
+    object _Object;
+
+    public RepoWrapper( object Object )
+    {
+      Set( Object );
+    }
+
+    public void Set( object Object )
+    {
+      _Object = Object;
+    }
+
+    public object Object()
+    {
+      return _Object;
+    }
+
+    public Type Type()
+    {
+      return _Object.GetType();
+    }
+  }
+}
